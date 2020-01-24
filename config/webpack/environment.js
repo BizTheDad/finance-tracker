80d82b5145cb4ec73e3f20cc122b0aa95c8e2143
Rails Udemy Course <justin.paro@gmail.com>
2020-01-24 11:57:10 -0600

    added Devise Bootstrap views

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
+
+
+// "jquery" and "popper.js" are dependencies of Bootstrap
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
