762ee0636caba5516e64859db1a9feb0406c92fe
Rails Udemy Course <justin.paro@gmail.com>
2020-01-26 00:40:56 -0600

    added user tracking of stocks, restrictions and removing stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -12,8 +16,12 @@ class Stock < ApplicationRecord
         last_price: client.price(ticker_symbol)
       )
     rescue => exception
-      return nil
+      nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 
 end
